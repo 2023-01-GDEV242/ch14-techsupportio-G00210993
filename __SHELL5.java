e7e817ff647da71dc34a4ad1ba864bb253bdb523
G00210993 <123398744+G00210993@users.noreply.github.com>
2023-04-30 18:33:40 -0400

    step 2

---
+++ __SHELL5.java
@@ -0,0 +1,10 @@
+
+public class __SHELL5 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("C:\\Users\\micha\\Documents\\GitHub\\ch14-techsupportio-G00210993");
+final SupportSystem supportS1 = (SupportSystem)__bluej_runtime_scope.get("supportS1");
+
+
+supportS1.start();
+
+}}
